231a2680d4ec68ae036a432ea789a5b841fc00dd
hamudmoha <hamudmoha11@gmail.com>
2024-06-22 19:54:13 +0300

    I add the unsulved sudoku row and column

--- sudko.py
+++ sudko.py
@@ -19,3 +19,23 @@ def find_empty_cell(board):
       if board[row][col] == 0:
         return row, col
   return None
+
+
+def is_valid(board, row, col, num):
+  """Checks if placing a number in a cell is valid according to Sudoku rules."""
+  # Check row
+  for i in range(9):
+    if board[row][i] == num and col != i:
+      return False
+  # Check column
+  for i in range(9):
+    if board[i][col] == num and row != i:
+      return False
+  # Check subgrid
+  start_row = (row // 3) * 3
+  start_col = (col // 3) * 3
+  for i in range(3):
+    for j in range(3):
+      if board[start_row + i][start_col + j] == num and (row, col) != (start_row + i, start_col + j):
+        return False
+  return True
